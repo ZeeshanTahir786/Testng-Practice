57ba73685b7e95b87866e683220cda6d165f01a9
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-25 19:53:09 +0500

    @DataProvider

--- src/test/Base2.java
+++ src/test/Base2.java
@@ -3,6 +3,7 @@ package test;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Base2 {
@@ -30,5 +31,29 @@ public class Base2 {
 	@BeforeMethod
 	public void BeforeMethod() {
 		System.out.println("i will execute before every method of class Base2");
+	}
+
+	@Test(dataProvider = "getData")
+	public void DataProvider(String username,String password) {
+		System.out.println("BfSiute");
+		System.out.println(username);
+		System.out.println(password);
+	}
+
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[3][2]; // 3 rows and 2 columns
+//		1st set
+		data[0][0] = "username1";
+		data[0][1] = "password1";
+
+//		2nd set
+		data[1][0] = "username2";
+		data[1][1] = "password2";
+//		3rd set
+		data[2][0] = "username3";
+		data[2][1] = "password3";
+
+		return data;
 	}
 }
